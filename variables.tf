3df0a4d03e8302693f4b4f0af924a5801a26a255
azhan92 <118318226+azhan92@users.noreply.github.com>
2023-09-11 14:44:19 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "crn" {
+  type        = string
+  description = "Power Systems Virtual Server CRN"
+}
+variable "memory" {
+  type        = number
+  default     = 4
+  description = "The amount of memory that you want to assign to your instance in gigabytes"
+}
+variable "processors" {
+  type        = number
+  default     = 0.25 
+  description = "The number of vCPUs to assign to the VM as visible within the guest Operating System"
+}
+variable "instance_name" {
+  type        = string
+  default     = "my_test_vm"
+  description = "The name to assign to the instance"
+}
+variable "processor_type" {
+  type        = string
+  default     = "shared"
+  description = "The type of processor mode in which the VM will run with 'shared', 'capped' or 'dedicated'"
+}
+variable "sys_type" {
+  type        = string
+  default     = "s922"
+  description = "The type of system on which to create the VM: 's922', 'e880', 'e980', 'e1080', or 's1022'"
+}
+variable "storage_type" {
+  type        = string
+  default     = "tier1"
+  description = "The type of storage tier to assign for storage volume performance: 'tier1' or 'tier3'"
+}
+variable "ssh_key_name" {
+  type        = string
+  description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
+variable "network_name" {
+  type        = string
+  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
+
+variable "placement_group" {
+  type        = string
+  default     = "None"
+  description = "The server placement group name where the instance will be placed, as defined for the selected Power Systems Virtual Server CRN"
+}
