81332f400bf36489296b7cb078ffd3c6b5ffdc66
azhan92 <alisonxzhang@gmail.com>
2024-04-29 15:38:43 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,10 +45,6 @@ variable "placement_group" {
   default     = "None"
   description = "The server placement group name where the instance will be placed, as defined for the selected Power Systems Virtual Server CRN"
 }
-variable "service_instance_id" {
-  type        = string
-  description = "The GUID of the service instance associated with an account"
-}
 variable "rhcos_image_name" {
   type        = string
   default     = "openxltile-v17-01-01" 
